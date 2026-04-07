c7ebc42ab00aeda38f3edf8e3e6c663c8f46d8c6
Eric Yu <zbrachinara@gmail.com>
2026-04-07 01:11:38 -0400

    rewrite using seq in place of map

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -5,8 +5,6 @@ use vstd::cell::CellId;
 use vstd::prelude::*;
 use vstd::simple_pptr::{self as pptr, PPtr};
 
-use crate::wo::*;
-
 verus! {
 
 #[repr(transparent)]
@@ -21,109 +19,105 @@ type CellPerms = Map<nat, pcell::PointsTo<PPtr<Node>>>;
 type PPtrPerms = Map<nat, pptr::PointsTo<Node>>;
 
 struct Permissions {
-    cell : Map<nat, pcell::PointsTo<PPtr<Node>>>,
-    pptr : Map<nat, pptr::PointsTo<Node>>,
+    cell : Seq<Option<pcell::PointsTo<PPtr<Node>>>>,
+    pptr : Seq<Option<pptr::PointsTo<Node>>>,
 }
 
 impl Permissions {
-    spec fn contains(&self, ix : nat) -> bool {
-        let this_cell_perm = self.cell.index(ix);
-        let this_ptr = this_cell_perm.value();
-
-        // This list has authoritative access to the node,
-        &&& self.cell.contains_key(ix)
-        // If the pointer's address is null,
-        &&& this_ptr.addr() == 0 ==> {
-            // The pointee is not initialized because its address is null,
-            // and no future pointee can be initialized either.
-            &&& forall|ix1 : nat| ix <= ix1 ==> !self.pptr.contains_key(ix1)
-            // On the other hand, this cell is initialized (we are reading from it),
-            // but all future cells are not initialized.
-            &&& forall|ix1 : nat| ix < ix1 ==> !self.cell.contains_key(ix1)
+    /// Conditions on which we rely when we own a link in the list
+    spec fn contains(&self, ix : int) -> bool {
+        // We must own the allocation
+        &&& self.cell.index(ix) matches Some(this_cell_perm)
+        // Suppose that the pointer is null
+        &&& this_cell_perm.value().addr() == 0 ==> {
+            // Then there must be no allocations beyond this pointer.
+            self.pptr.len() == ix && self.cell.len() == ix + 1
         }
-        // If the pointer points to another node,
-        &&& this_ptr.addr() != 0 ==> {
-            let this_ptr_perm = self.pptr.index(ix);
-            let next_cell_perm = self.cell.index(ix + 1);
-
-            // The list has authoritative index to the pointee
-            &&& self.pptr.contains_key(ix)
-            // The pointee is pointed to by the pointer contained in this cell
-            &&& this_ptr_perm.pptr() == this_ptr
-            // The pointer is initialized
-            &&& this_ptr_perm.mem_contents() matches pptr::MemContents::Init(next_node)
-            // And we have permissions for the pointee
-            &&& next_node.next.id() == next_cell_perm.id()
+        // Suppose instead that the pointer is nonnull
+        &&& this_cell_perm.value().addr() != 0 ==> {
+            // Then we hold a pointer at this index
+            &&& self.pptr.index(ix) matches Some(this_pptr_perm)
+            // That pointer must be the same as the one in the cell
+            &&& this_pptr_perm.pptr() == this_cell_perm.value()
+            // The pointee must be initialized
+            &&& this_pptr_perm.mem_contents() matches pptr::MemContents::Init(next_node)
+            // In fact, the next cell must also be initialized
+            &&& self.cell.index(ix + 1) matches Some(next_node_perm)
+            // And the pointee must be exactly the next cell
+            &&& next_node.next.id() == next_node_perm.id()
         }
     }
 
-    spec fn based_at(self, ix : nat, permission : CellId) -> bool {
-        self.contains(ix) && self.cell[ix].id() == permission
+    spec fn based_at(self, ix : int, permission : CellId) -> bool {
+        self.contains(ix) && self.cell[ix].unwrap().id() == permission
     }
 
-    spec fn last(self, ix : nat) -> bool {
-        self.contains(ix) && self.cell[ix].value().addr() == 0
+    // spec fn last(self, ix : nat) -> bool {
+    //     self.contains(ix) && self.cell[ix].value().addr() == 0
+    // }
+
+    spec fn len(self) -> nat {
+        self.cell.len()
     }
 
     spec fn mirrors(self, permission : CellId) -> bool {
         &&& self.based_at(0, permission)
-        &&& forall|ix : nat| self.contains(ix) && !self.last(ix) ==> self.contains(ix + 1)
-        &&& exists|ix : nat| self.last(ix)
+        &&& forall|ix| self.contains(ix) && ix < self.len() ==> #[trigger] self.contains(ix + 1)
     }
 
-    spec fn len(self) -> nat {
-        wo(|ix| self.last(ix))
-    }
+    // spec fn len(self) -> nat {
+    //     wo(|ix| self.last(ix))
+    // }
 
-    proof fn len_def(&self)
-    requires exists|c| self.mirrors(c),
-    ensures
-        forall|y : nat| y < self.len() ==> !self.last(y),
-        self.last(self.len())
-    {
-        let is_last = |ix| self.last(ix);
-        assert(is_last(choose|ix| self.last(ix)));
-        wo_proof(is_last);
-        assert forall|y : nat| y < self.len() implies !self.last(y) by {
-            assert(!is_last(y));
-        }
-    }
+    // proof fn len_def(&self)
+    // requires exists|c| self.mirrors(c),
+    // ensures
+    //     forall|y : nat| y < self.len() ==> !self.last(y),
+    //     self.last(self.len())
+    // {
+    //     let is_last = |ix| self.last(ix);
+    //     assert(is_last(choose|ix| self.last(ix)));
+    //     wo_proof(is_last);
+    //     assert forall|y : nat| y < self.len() implies !self.last(y) by {
+    //         assert(!is_last(y));
+    //     }
+    // }
 
-    proof fn must_contain(&self, y : nat)
-    requires exists|c| self.mirrors(c), y <= self.len()
-    ensures self.contains(y)
-    decreases y
-    {
-        if (y > 0) {
-            let prev = (y - 1) as nat;
+    // proof fn must_contain(&self, y : nat)
+    // requires exists|c| self.mirrors(c), y <= self.len()
+    // ensures self.contains(y)
+    // decreases y
+    // {
+    //     if (y > 0) {
+    //         let prev = (y - 1) as nat;
 
-            self.len_def();
-            assert(!self.last(prev));
+    //         self.len_def();
+    //         assert(!self.last(prev));
 
-            self.must_contain(prev);
-            assert(self.contains(prev));
-        }
-    }
+    //         self.must_contain(prev);
+    //         assert(self.contains(prev));
+    //     }
+    // }
 
-    proof fn has_pointer(&self, y : nat)
-    requires
-        exists|c| self.mirrors(c),
-        self.pptr.contains_key(y) || y < self.len()
-    ensures
-        self.pptr.contains_key(y),
-        y < self.len(),
-    {
-        self.len_def();
-        if (y < self.len()) {
-            self.must_contain(y);
-        }
-    }
+    // proof fn has_pointer(&self, y : nat)
+    // requires
+    //     exists|c| self.mirrors(c),
+    //     self.pptr.contains_key(y) || y < self.len()
+    // ensures
+    //     self.pptr.contains_key(y),
+    //     y < self.len(),
+    // {
+    //     self.len_def();
+    //     if (y < self.len()) {
+    //         self.must_contain(y);
+    //     }
+    // }
 }
 
 struct List {
     first : PCell<PPtr<Node>>,
-    cell_perms : Tracked<Map<nat, pcell::PointsTo<PPtr<Node>>>>,
-    pptr_perms : Tracked<Map<nat, pptr::PointsTo<Node>>>,
+    cell_perms : Tracked<Seq<Option<pcell::PointsTo<PPtr<Node>>>>>,
+    pptr_perms : Tracked<Seq<Option<pptr::PointsTo<Node>>>>,
 }
 
 impl List {
@@ -135,17 +129,16 @@ impl List {
     }
     pub closed spec fn wf(self) -> bool { self.permissions().mirrors(self.first.id()) }
     pub closed spec fn len(self) -> nat { self.permissions().len() }
-    pub closed spec fn node_at(self, node_ptr : PPtr<Node>, index : nat) -> bool {
-        index < self.len() && self.pptr_perms@[index].pptr() == node_ptr
+    pub closed spec fn node_at(self, node_ptr : PPtr<Node>, index : int) -> bool {
+        index < self.len() && self.pptr_perms@[index].unwrap().pptr() == node_ptr
     }
     pub open spec fn contains_node(self, node_ptr : PPtr<Node>) -> bool {
-        exists|y : nat| self.node_at(node_ptr, y)
+        exists|y| self.node_at(node_ptr, y)
     }
-    pub open spec fn index_of(self, node_ptr : PPtr<Node>) -> nat
+    pub open spec fn index_of(self, node_ptr : PPtr<Node>) -> int
     recommends self.contains_node(node_ptr)
     {
         choose|y| self.node_at(node_ptr, y)
     }
-
 }
 }
